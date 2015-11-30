eadcfe3dc4f9a24ae7cab3a76902c25ea9196809
zeropv <paul.nalivanniy@gmail.com>
2015-11-30 18:13:28 +0200

    js homework 1

---
+++ js/lesson01/js/script1.js
@@ -0,0 +1,26 @@
+
+function IsNumeric(input)
+{
+    return (input - 0) == input && (''+input).trim().length > 0;
+}
+function pow (a,b) {
+var result=1;
+	for (var i = 1; i <= b; i++){
+		debugger;
+		result *= a;
+	}
+	console.log ('The result of num POW exp is:', result);
+}
+alert ('1 homework part...');
+var num, exp;
+console.log(IsNumeric(12));
+do{
+	num = prompt ('Enter the Number:');
+}
+while(!IsNumeric(num));
+	
+do{
+	exp = prompt ('Enter the Exponent:');
+}
+while(!IsNumeric(num));
+pow (num, exp);
---
+++ js/lesson01/js/script2.js
@@ -0,0 +1,26 @@
+alert ('second part of homework');
+var n = 5;
+var names = [];
+var isRegistered = false;
+var msg, str;
+for (var i=0; i<n; i++){
+	msg="Input name #"+ (i+1);
+	str = prompt(msg);
+	names.push(str);
+}
+var userName;
+userName = prompt("Log in:");
+i=0;
+while ((i<n) && (!isRegistered)){
+	debugger;
+	isRegistered = (names[i] == userName);
+	i++; 
+
+}
+if (isRegistered) {
+	msg = userName.toUpperCase() + ", you have been succesfull loggining...";
+	alert (msg);
+} else{
+	msg = userName.toUpperCase() + ", you are not registered! Access denied.";
+	alert (msg);
+}
